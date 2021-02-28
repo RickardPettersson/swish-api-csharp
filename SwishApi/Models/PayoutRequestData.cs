56b20763e8624bed1acca72a5a43aba5f7dd08d5
Pierre Schönbeck <pierre.schonbeck@krsystem.se>
2021-02-28 21:06:39 +0100

    Lagt till modeller för Payout

---
+++ SwishApi/Models/PayoutRequestData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwishApi.Models
+{
+    public class PayoutRequestData
+    {
+        public string payoutInstructionUUID { get; set; }
+        public string payerPaymentReference { get; set; }
+        public string payerAlias { get; set; }
+        public string payeeAlias { get; set; }
+        public string payeeSSN { get; set; }
+        public string amount { get; set; }
+        public string currency { get; set; }
+        public string payoutType { get; set; }
+        public string message { get; set; }
+        public string instructionDate { get; set; }
+        public string signingCertificateSerialNumber { get; set; }
+    }
+}
---
+++ SwishApi/Models/PayoutRequestResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwishApi.Models
+{
+    public class PayoutRequestResponse
+    {
+        public string Error { get; set; }
+        //public string Location { get; set; }
+    }
+}
